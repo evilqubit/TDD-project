470e0f012ca0bfd2e2b449d2cf5186dfae0b98a8
Carlo Khanati <carlo.khanati@gmail.com>
2016-08-26 17:52:40 +0300

    test async

--- express.js
+++ express.js
@@ -15,12 +15,50 @@ app.get('/',function(req,res){
   res.send('Please select a collection');
 });
 
-
+var s
 
 
 const PORT = 8080
 app.listen(PORT,function(){
   console.log("app is listening on port " + PORT);
 })
+
+ var async = require('async');
+  var age = 10;
+  var sex = 'm';
+  var name ='';
+  var newname ='';
+app.get('/promise', function(req, res) {
+async.series([
+    function(callback) {
+        name = 'billy';
+        callback();
+    },
+    function(callback){
+      if(age == 10 && sex === 'm' && name === 'billy') {
+        newname = 'billy bob';
+         callback();
+        }
+    else{
+        callback(new Error('not found'));
+    }
+       
+    },
+    function(callback){
+         if(newname !== 'billy bob')
+             return callback(new Error('test'));
+        callback();
+    },
+    function(callback){
+     res.status(200);
+      res.send('Take your money bitch');
+      res.end();
+    }
+],function(err){
+     res.status(500);
+     res.send('err');
+     res.end();
+});
+});
 
 module.exports = app;
